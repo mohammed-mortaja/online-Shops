044968e79c005b2f47197bd30bd531b4699d8c73
mohammed-mortaja <sala.moh611@gmailcom>
2023-03-21 12:50:33 +0200

    Committing local changes before merging with remote repository

--- resources/views/dashboard/parent.blade.php
+++ resources/views/dashboard/parent.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>MY-Store | @yield('title')</title>
+  <title>{{ config('app.name')}} | @yield('title')</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
@@ -165,9 +165,9 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{ asset('dashboard/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="{{ asset('dashboard/dist/img/logo-store.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">تسوق عبر الإنترنت    </span>
+      <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
     <!-- Sidebar -->
@@ -178,7 +178,7 @@
           <img src="{{ asset('dashboard/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">محمد مرتجى </a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -525,6 +525,9 @@
             <ol class="breadcrumb float-sm-left">
                 <li class="breadcrumb-item"><a href="{{ route('parent') }}">الرئيسية</a></li>
                 <li class="breadcrumb-item active">@yield('sub-title')</li>
+                {{-- @section('breadcrumb')
+                <li class="breadcrumb-item"><a href="{{ route('parent') }}">الرئيسية</a></li>
+                @show --}}
             </ol>
         </div><!-- /.col -->
         <div class="col-sm-6">
